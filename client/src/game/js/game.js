f44d441f5a579db3eef9784769bec45cdf4958a1
Guy Anonymous <guya0265@gmail.com>
2026-08-13 21:00:22 -0400

    Imported sidebar HTML and CSS. Needed to reference behavior.

--- client/src/game/js/game.js
+++ client/src/game/js/game.js
@@ -1,12 +1,14 @@
 import { initDeckActions } from "./gameboard/logic/loader.js";
 import { initUI } from "./gameboard/ui/ui.js";
 
-import { initPileBrowser } from "./gameboard/ui/pileBrowser.js";
-import { initCardView } from "./gameboard/ui/cardZoom.js";
+import { initPileBrowser } from "./gameboard/ui/overlays/pileBrowser.js";
+import { initCardView } from "./gameboard/ui/overlays/cardZoom.js";
+import { initSiderbar } from "./sidebar/sidebar.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     initUI();
     initDeckActions();
     initPileBrowser();
     initCardView();
+    initSiderbar();
 });
