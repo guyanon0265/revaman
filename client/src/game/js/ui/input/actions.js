bebb2ec5ebb1228f0eabec15eeff70c823349ddf
Guy Anonymous <guya0265@gmail.com>
2026-08-18 12:16:58 -0400

    Added more actions in actions.js. Abstraction underway.

--- client/src/game/js/ui/input/actions.js
+++ client/src/game/js/ui/input/actions.js
@@ -1,12 +1,68 @@
 import * as lengine from '../../logic/loggingEngine.js';
-import { clearSelection, runtimeState } from '../../logic/state.js';
+import {
+  clearSelection,
+  clientState,
+  runtimeState,
+} from '../../logic/state.js';
+import { requestRedo, requestUndo } from '../../networkSync.js';
+import { closeAllOverlays } from '../overlays/overlays.js';
 import { refreshPileBrowser } from '../overlays/pileBrowser.js';
 import { renderEntireBoard } from '../render.js';
 
+export function showOppHand() {
+  const btnShowOppHand = document.getElementById('btn-show-opp-hand');
+  clientState.showOpponentHand = !clientState.showOpponentHand;
+
+  btnShowOppHand.textContent = clientState.showOpponentHand
+    ? 'Hide Opponent Hand'
+    : 'Show Opponent Hand';
+
+  renderEntireBoard();
+}
+
+export function shuffleDeck() {
+  lengine.shuffleZone(`${runtimeState.mySlot}-deck`);
+}
+
+export function moveSelectedCardToZone(zone) {
+  lengine.moveCardToZone(
+    clientState.selectedInstanceId,
+    clientState.selectedZone,
+    zone
+  );
+  clearSelection();
+  renderEntireBoard();
+  refreshPileBrowser();
+}
+
 export function drawCardFromZone(zone) {
-  lengine.drawCards(zone, `${runtimeState.mySlot}-hand`, 1);
+  const ownerSlot = zone.split('-')[0];
+
+  lengine.drawCards(zone, `${ownerSlot}-hand`, 1);
 
   clearSelection();
   renderEntireBoard();
   refreshPileBrowser();
+}
+
+export function undoAction() {
+  if (runtimeState.mode === 'multiplayer') {
+    requestUndo();
+    return;
+  }
+  if (lengine.undo()) {
+    closeAllOverlays();
+    renderEntireBoard();
+  }
+}
+
+export function redoAction() {
+  if (runtimeState.mode === 'multiplayer') {
+    requestRedo();
+    return;
+  }
+  if (lengine.redo()) {
+    closeAllOverlays();
+    renderEntireBoard();
+  }
 }
